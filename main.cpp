15ab9da9f62ebd1a9eb7c90b318544f45dc9f3dd
Mike Ulicny <michaeaulicny@gmail.com>
2019-01-13 22:12:20 -0500

    added main file

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//system defined classes
+#include <iostream>
+#include <curl/curl.h>
+
+//api classes
+#include "account.hpp"
+
+
+
+int main(void)
+{	
+	std::string USER_INPUT;
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	std::cout << "Algo Trader Version 1.0" << std::endl;
+	
+	api::account user;
+	user.getQuotes("NVDA");
+	std::cin.get();
+
+	curl_global_cleanup();
+	return 0;
+}
+
