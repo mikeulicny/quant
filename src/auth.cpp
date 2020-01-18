fe730a6f4dbb402609b424d4639ca151d1daa82c
Mike Ulicny <michaeaulicny@gmail.com>
2020-01-18 01:36:22 -0500

    changed to nholmann/json.hpp for json handling. Updated member functions for new json

--- src/auth.cpp
+++ src/auth.cpp
@@ -1,36 +1,62 @@
-#include "tokens.hpp"
+#include "auth.hpp"
 
 tdma::auth::auth()
 {
-    FILE* fp = fopen("credentials.json", "r");
-    if (fp)
-    {
-        char r_buffer[65536];
-        rapidjson::FileReadStream is(fp, r_buffer, sizeof(r_buffer));
-        rapidjson::Document doc;
-        doc.ParseStream(is);
-        client_id = doc["consumer_key"].GetString();
-        access_token = doc["access_token"].GetString();
-        refresh_token = doc["refresh_token"].GetString();
-    }
-    fclose(fp);
+    std::ifstream file("credentials.json");
+    json temp_json;
+    file >> temp_json;
+
+    client_id = temp_json["client_id"].get<std::string>();
+    m_access_token = temp_json["access_token"].get<std::string>();
+    m_refresh_token = temp_json["refresh_token"].get<std::string>();
+    access_expires_at = temp_json["access_expires_at"].get<std::chrono::steady_clock::time_point>();
+    refresh_expires_at = temp_json["refresh_expires_at"].get<std::chrono::steady_clock::time_point>();
 }
 
 tdma::auth::~auth()
 {
-    // load data to database
+    write_to_file();
 }
 
 void tdma::auth::post(const bool &get_refresh)
 {
     // request new codes
     curl_handle.setopt(CURLOPT_URL, "https://api.tdameritrade.com/v1/oauth2/token");
-    curl_handle.setopt(CURLOPT_NOPROGRESS, 1L);
     curl_handle.setopt(CURLOPT_POSTFIELDS, post_data.c_str());
     curl_handle.setopt(CURLOPT_POSTFIELDSIZE, (long)std::strlen(post_data.c_str()));
     curl_handle.setopt(CURLOPT_CUSTOMREQUEST, "POST");
 
     curl_handle.perform();
+    
+    if (curl_handle.result() != CURLE_OK)
+    {
+        return;
+    }
+
+    temp_json = json::parse(curl_handle.data());
+    
+    client_id = temp_json["client_id"].get<std::string>();
+    m_access_token = temp_json["access_token"].get<std::string>();
+    access_expires_at = std::chrono::steady_clock::now() + std::chrono::minutes(29);
+
+    if (get_refresh)
+    {
+        m_refresh_token = temp_json["refresh_token"].get<std::string>();
+        refresh_expires_at = std::chrono::steady_clock::now() + std::chrono::hours(2159);
+    }
+    
+}
+
+void tdma::auth::write_to_file()
+{
+    std::ofstream file("credentials.json");
+    json temp_json;
+    temp_json["client_id"] = m_client_id;
+    temp_json["access_token"] = m_access_token;
+    temp_json["refresh_token"] = m_refresh_token;
+    temp_json["access_expires_at"] = access_expires_at;
+    temp_json["refresh_expires_at"] = refresh_expires_at;
+    file << temp_json << std::endl;
 }
 
 const bool tmda::auth::is_expired()
--- src/auth.hpp
+++ src/auth.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <chrono>
 #include <string>
 #include <curl/curl.h>
-#include <rapidjson/document.h>
+#include <json.hpp>
 #include <fmt/format.h>
 
 #include "unique_curl.hpp"
@@ -18,18 +19,20 @@ class auth
         ~auth();
         
         void post();
+        void write_to_file();
         const bool is_expired();
-
+        
         const std::string &client_id() const {return m_client_id;}
         const std::string &access_token() const {return m_access_token;}
         const std::string &refresh_token() const {return m_refresh_token;}
 
     private:
-        rapidjson::Document doc;
-        
+
         std::string m_client_id;
         std::string m_access_token;
         std::string m_refresh_token;
+        std::chrono::steady_clock::time_point access_expires_at;
+        std::chrono::steady_clock::time_point refresh_expires_at;
 
         unique_curl curl_handle;
 
