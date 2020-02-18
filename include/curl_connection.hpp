2bdfde989fa5fef22242fa510ac7bfdd074aea32
Mike Ulicny <michaeaulicny@gmail.com>
2020-02-17 23:42:41 -0500

    curl raii class name changed to curl_connection for better naming convention regarding class inheritance

--- include/curl_connection.hpp
+++ include/curl_connection.hpp
@@ -6,20 +6,22 @@
 namespace tdma 
 {
 
-class unique_curl
+class curl_connection
 {
     public:
-        unique_curl();
-        ~unique_curl();
+        curl_connection();
+
+        virtual ~curl_connection();
        
         // raii class requirements
-        unique_curl(const unique_curl&) = delete;
-        unique_curl &operator=(const unique_curl&) = delete;
+        curl_connection(const curl_connection&) = delete;
+
+        curl_connection &operator=(const curl_connection&) = delete;
 
         template<typename T>
         void setopt(const CURLoption &option, const T &parameter)
         {
-            curl_easy_setopt(curl, option, parameter);
+            curl_easy_setopt(curl_handle, option, parameter);
         }
 
         void reset();
@@ -27,10 +29,18 @@ class unique_curl
         void perform();
         
         const CURLcode &result() const { return res; }
+
         const std::string &data() const { return write_buffer; }
     
     private:
-        CURL *curl;
+        static struct curl_init 
+        {
+            // curl global environment handler
+            curl_init() { curl_global_init(CURL_GLOBAL_ALL); }
+            ~curl_init() { curl_global_cleanup(); }
+        }curl_environment;
+
+        CURL *curl_handle;
         CURLcode res;
         std::string write_buffer;
         static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
---
+++ src/curl_connection.cpp
@@ -0,0 +1,38 @@
+#include "../include/unique_curl.hpp"
+
+
+curl_connection::curl_init curl_connection::curl_environment;
+
+tdma::curl_connection::curl_connection() : curl_handle(curl_easy_init()), res(nullptr)
+{
+}
+
+tdma::curl_connection::~curl_connection()
+{
+    curl_easy_cleanup(curl_handle);
+}
+
+void tdma::curl_connection::reset()
+{
+    write_buffer.clear();
+    curl_easy_reset(curl_handle);
+}
+
+void tdma::curl_connection::perform()
+{
+    write_buffer.clear();
+
+    // default curl settings
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &write_buffer);
+
+    res = curl_easy_perform(curl_handle);
+}
+
+size_t tdma::curl_connection::write_callback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
