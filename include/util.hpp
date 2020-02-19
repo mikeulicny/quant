9f9d402551c2a733bc992554e11e29a0d16daaff
Mike Ulicny <michaeaulicny@gmail.com>
2020-02-18 23:22:40 -0500

    removed inline from url_encode and url_decode functions. implemented to_upper c++ function

--- include/util.hpp
+++ include/util.hpp
@@ -9,6 +9,10 @@
 namespace util 
 {
 
+const std::string url_encode(const std::string &s);
+
+const std::string url_decode(const std::string &str);
+
 inline unsigned char to_hex(unsigned char x)
 {
     return x + (x > 9 ? ('A' - 10) : '0');
@@ -27,53 +31,11 @@ inline unsigned char from_hex(unsigned char ch)
     return ch;
 }
 
-inline const std::string url_encode(const std::string &s)
+inline std::string to_upper(std::string str)
 {
-    std::ostringstream os;
-    for (std::string::const_iterator ci = s.begin(); ci != s.end(); ++ci)
-    {
-        if ((*ci >= 'a' && *ci <= 'z') ||
-            (*ci >= 'A' && *ci <= 'Z') ||
-            (*ci >= '0' && *ci <= '9'))
-        { // allowed
-            os << *ci;
-        }
-        else if (*ci == ' ')
-        {
-            os << '+';
-        }
-        else
-        {
-            os << '%' << to_hex(*ci >> 4) << to_hex(*ci % 16);
-        }
-    }
-    return os.str();
+    std::transform(str.begin(), str.end(), str.begin(),
+            [](unsigned char c){ return std::toupper(c); } );
+    return str;
 }
 
-inline const std::string url_decode(const std::string &str)
-{
-    std::string result;
-    std::string::size_type i;
-    for (i = 0; i < str.size(); ++i)
-    {
-        if (str[i] == '+')
-        {
-            result += ' ';
-        }
-        else if (str[i] == '%' && str.size() > i + 2)
-        {
-            const unsigned char ch1 = from_hex(str[i+1]);
-            const unsigned char ch2 = from_hex(str[i+2]);
-            const unsigned char ch = (ch1 << 4) | ch2;
-            result += ch;
-            i += 2;
-        }
-        else
-        {
-            result += str[i];
-        }
-    }
-    return result;
-}
-
-} // http_util namespace
+} // util namespace
---
+++ src/util.cpp
@@ -0,0 +1,50 @@
+#include "../include/util.hpp"
+
+const std::string url_encode(const std::string &s)
+{
+    std::ostringstream os;
+    for (std::string::const_iterator ci = s.begin(); ci != s.end(); ++ci)
+    {
+        if ((*ci >= 'a' && *ci <= 'z') ||
+            (*ci >= 'A' && *ci <= 'Z') ||
+            (*ci >= '0' && *ci <= '9'))
+        { // allowed
+            os << *ci;
+        }
+        else if (*ci == ' ')
+        {
+            os << '+';
+        }
+        else
+        {
+            os << '%' << to_hex(*ci >> 4) << to_hex(*ci % 16);
+        }
+    }
+    return os.str();
+}
+
+const std::string url_decode(const std::string &str)
+{
+    std::string result;
+    std::string::size_type i;
+    for (i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '+')
+        {
+            result += ' ';
+        }
+        else if (str[i] == '%' && str.size() > i + 2)
+        {
+            const unsigned char ch1 = from_hex(str[i+1]);
+            const unsigned char ch2 = from_hex(str[i+2]);
+            const unsigned char ch = (ch1 << 4) | ch2;
+            result += ch;
+            i += 2;
+        }
+        else
+        {
+            result += str[i];
+        }
+    }
+    return result;
+}
