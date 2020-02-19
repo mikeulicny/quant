e24074f20641e8097ef81528b4737f5c23e0f267
Mike Ulicny <michaeaulicny@gmail.com>
2020-02-18 23:35:30 -0500

    fixed url_encode and url_decode undefined reference errors

--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,6 @@
 #include "../include/util.hpp"
 
-const std::string url_encode(const std::string &s)
+const std::string util::url_encode(const std::string &s)
 {
     std::ostringstream os;
     for (std::string::const_iterator ci = s.begin(); ci != s.end(); ++ci)
@@ -17,13 +17,13 @@ const std::string url_encode(const std::string &s)
         }
         else
         {
-            os << '%' << to_hex(*ci >> 4) << to_hex(*ci % 16);
+            os << '%' << util::to_hex(*ci >> 4) << util::to_hex(*ci % 16);
         }
     }
     return os.str();
 }
 
-const std::string url_decode(const std::string &str)
+const std::string util::url_decode(const std::string &str)
 {
     std::string result;
     std::string::size_type i;
@@ -35,8 +35,8 @@ const std::string url_decode(const std::string &str)
         }
         else if (str[i] == '%' && str.size() > i + 2)
         {
-            const unsigned char ch1 = from_hex(str[i+1]);
-            const unsigned char ch2 = from_hex(str[i+2]);
+            const unsigned char ch1 = util::from_hex(str[i+1]);
+            const unsigned char ch2 = util::from_hex(str[i+2]);
             const unsigned char ch = (ch1 << 4) | ch2;
             result += ch;
             i += 2;
