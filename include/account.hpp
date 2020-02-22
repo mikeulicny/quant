74d682a39c3571fc082b3d8e7cfb019b26f72845
Mike Ulicny <michaeaulicny@gmail.com>
2020-02-22 12:17:59 -0500

    added set flag member functions for account object

--- include/account.hpp
+++ include/account.hpp
@@ -17,6 +17,10 @@ class account : private unique_curl
         account(auth &auth_ref, const std::string &account_id, const bool positions, const bool orders);
         ~account();
 
+        void set_position(bool position_flag) { m_positions = position_flag; }
+
+        void set_order(bool order_flag) { m_orders = order_flag; }
+
         // sends a curl request to retreive account details
         const nlohmann::json get();
 
