a6623be10d4368ae4b73d962fa47bc3877eb9c74
Mike Ulicny <MichaelAUlicny@gmail.com>
2020-02-29 12:34:22 -0500

    changed function parameters back to strings

--- include/price_history.hpp
+++ include/price_history.hpp
@@ -9,64 +9,63 @@
 #include <auth.hpp>
 #include <util.hpp>
 
+// TODO: implement finance namespace for constexpr variables
+
 namespace tdma
 {
-
-enum period_type 
-{
-    DAY,
-    MONTH,
-    YEAR,
-    YTD
-};
-
-enum frequency_type
-{
-    MINUTE,
-    DAILY,
-    WEEKLY,
-    MONTHLY
-};
 
 class price_history : private curl_connection
 {
     public:
-        
+       
         price_history(auth &auth_ref, const std::string &symbol);
 
-        price_history(auth &auth_ref, const std::string &symbol,
-                int period_value, period_type period, int frequency_value, frequency_type frequency);
+        price_history(auth &auth_ref,
+                const std::string &symbol,
+                const int period_value,
+                const std::string &period,
+                const int frequency_value,
+                const std::string &frequency);
 
-        price_history(auth &auth_ref, const std::string &symbol, long long start_timepoint, long long end_timepoint,
-                int frequency_value, frequency_type frequency);
+        price_history(auth &auth_ref,
+                const std::string &symbol,
+                const long long start_timepoint,
+                const long long end_timepoint,
+                const int frequency_value, 
+                const std::string &frequency);
 
         ~price_history();
 
         // sends a curl request to obtain price history
         const nlohmann::json get();
 
-        // set a new symbol, allowing the same curl handle to be used
+        // set a new symbol, this sets a new path in the url_handle only
         void set_symbol(const std::string &symbol);
 
         // TODO: set_timeframe(int, period_type, frequency_type);
 
         // set or change the timeframe using periods and frequency
-        void set_timeframe(int period_value, period_type period, int frequency_value, frequency_type frequency);
+        void set_timeframe(
+                const int period_value, 
+                const std::string &period, 
+                const int frequency_value, 
+                const std::string &frequency);
 
         // set or change timeframe using start date, end date, and frequency
-        void set_timeframe(long long start_date, long long end_date, int frequency_value, frequency_type frequency);
+        void set_timeframe(
+                const long long start_date, 
+                const long long end_date, 
+                const int frequency_value, 
+                const std::string &frequency);
 
         // set extended hours flag
-        void set_extended_hours(bool extended_hours) { m_extended_hours = extended_hours; }
+        void set_extended_hours(const bool extended_hours) { m_extended_hours = extended_hours; }
 
     private:
         auth *p_auth;
         std::string m_symbol;
         std::string m_url;
         bool m_extended_hours;
-
-        void set_period(period_type period);
-        void set_frequency(frequency_type frequency);
 
 };// price_history class
 
--- src/price_history.cpp
+++ src/price_history.cpp
@@ -1,6 +1,4 @@
-#include "../include/price_history.hpp"
-
-// TODO: think about changing enums to strings?
+#include <price_history.hpp>
 
 
 tdma::price_history::price_history(auth &auth_ref, const std::string &symbol) : p_auth(&auth_ref)
@@ -9,16 +7,26 @@ tdma::price_history::price_history(auth &auth_ref, const std::string &symbol) : 
     set_symbol(symbol);
 }
 
-tdma::price_history::price_history(auth &auth_ref, const std::string &symbol, int period_value, period_type period,
-        int frequency_value, frequency_type frequency) : p_auth(&auth_ref)
+tdma::price_history::price_history(auth &auth_ref,
+        const std::string &symbol, 
+        const int period_value, 
+        const std::string &period,
+        const int frequency_value, 
+        const std::string &frequency) 
+    : p_auth(&auth_ref)
 {
     curl_connection::url_set(CURLUPART_URL, TDMA_URL_BASE);
     set_symbol(symbol);
     set_timeframe(period_value, period, frequency_value, frequency);
 }
 
-tdma::price_history::price_history(auth &auth_ref, const std::string &symbol, long long start_timepoint, long long end_timepoint,
-        int frequency_value, frequency_type frequency) : p_auth(&auth_ref)
+tdma::price_history::price_history(auth &auth_ref, 
+        const std::string &symbol, 
+        const long long start_timepoint,
+        const long long end_timepoint,
+        const int frequency_value, 
+        const std::string &frequency) 
+    : p_auth(&auth_ref)
 {
     curl_connection::url_set(CURLUPART_URL, TDMA_URL_BASE);
     set_symbol(symbol);
@@ -49,63 +57,26 @@ void tdma::price_history::set_symbol(const std::string &symbol)
     curl_connection::url_set(CURLUPART_PATH, "/v1/marketdata/" + m_symbol + "/pricehistory");
 }
 
-void tdma::price_history::set_timeframe(int period_value, period_type period, int frequency_value, frequency_type frequency)
-{    /* allowed values for:
-     * period - day: 1, 2, 3, 4, 5, 10*
-     *          month: 1*, 2, 3, 6
-     *          year: 1*, 2, 3, 5, 10, 15, 20
-     *          ytd: 1*
-     */
+void tdma::price_history::set_timeframe(
+        const int period_value, 
+        const std::string &period, 
+        const int frequency_value, 
+        const std::string &frequency)
+{
     curl_connection::url_set(CURLUPART_QUERY, "period=" + std::to_string(period_value));
+    curl_connection::url_set(CURLUPART_QUERY, "periodType=" + period, CURLU_APPENDQUERY);
     curl_connection::url_set(CURLUPART_QUERY, "frequency=" + std::to_string(frequency_value), CURLU_APPENDQUERY);
-    set_period(period);
-    set_frequency(frequency);
+    curl_connection::url_set(CURLUPART_QUERY, "frequencyType=" frequency, CURLU_APPENDQUERY);
 }
 
-void tdma::price_history::set_timeframe(long long start_date, long long end_date, int frequency_value, frequency_type frequency)
+void tdma::price_history::set_timeframe(const long long start_date,
+        const long long end_date,
+        const int frequency_value, 
+        const std::string &frequency)
 {
-    /* start date and end date must be passed as milliseconds since epoch
-     */
+    // start date and end date must be passed as milliseconds since epoch
     curl_connection::url_set(CURLUPART_QUERY, "endDate=" + std::to_string(end_date));
     curl_connection::url_set(CURLUPART_QUERY, "startDate=" + std::to_string(start_date), CURLU_APPENDQUERY);
     curl_connection::url_set(CURLUPART_QUERY, "frequency=" + std::to_string(frequency_value), CURLU_APPENDQUERY);
-    set_frequency(frequency); 
-}
-
-void tdma::price_history::set_period(period_type period)
-{
-    switch (period)
-    {
-        case DAY:
-            curl_connection::url_set(CURLUPART_QUERY, "periodType=day", CURLU_APPENDQUERY);
-            break;
-        case MONTH:
-            curl_connection::url_set(CURLUPART_QUERY, "periodType=month", CURLU_APPENDQUERY);
-            break;
-        case YEAR:
-            curl_connection::url_set(CURLUPART_QUERY, "periodType=year", CURLU_APPENDQUERY);
-            break;
-        case YTD:
-            curl_connection::url_set(CURLUPART_QUERY, "periodType=ytd", CURLU_APPENDQUERY);
-            break;
-    }
-}
-
-void tdma::price_history::set_frequency(frequency_type frequency)
-{
-    switch (frequency)
-    {
-        case MINUTE:
-            curl_connection::url_set(CURLUPART_QUERY, "frequencyType=minute", CURLU_APPENDQUERY);
-            break;
-        case DAILY:
-            curl_connection::url_set(CURLUPART_QUERY, "frequencyType=daily", CURLU_APPENDQUERY);
-            break;
-        case WEEKLY: 
-            curl_connection::url_set(CURLUPART_QUERY, "frequencyType=weekly", CURLU_APPENDQUERY);
-            break;
-        case MONTHLY:
-            curl_connection::url_set(CURLUPART_QUERY, "frequencyType=monthly", CURLU_APPENDQUERY);
-            break;
-    }
+    curl_connection::url_set(CURLUPART_QUERY, "frequencyType=" + frequency, CURLU_APPENDQUERY);
 }
